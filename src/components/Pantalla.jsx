135f3ff382f8c5b42fb7a83f13ffe7d6bad97a98
Fabian Bermudez <eddybermudez200103@gmail.com>
2023-10-04 17:51:32 -0500

    Importando los archivos al repositorio

---
+++ src/components/Pantalla.jsx
@@ -0,0 +1,11 @@
+import '../css/Pantalla.css'
+
+const Pantalla = ({input}) => {
+    return(
+        <div className="input">
+            {input}
+        </div>
+    )
+}
+
+export default Pantalla
